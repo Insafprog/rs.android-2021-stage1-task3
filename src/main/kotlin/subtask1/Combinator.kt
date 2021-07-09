e35930942567f6e0afd4339cd0b655208c99ee76
Insafproger <BaKiR007>
2021-07-09 07:55:56 +0300

    First

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,20 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val x = array[0]
+        val n = array[1]
+        if(x < 0 || n <= 0) return null
+        for (m in 1 until n) {
+            if (binom(n, m) >= x) return m
+        }
+        return null
     }
+
+    private fun factorial(n: Int): Long = if (n < 2) 1L else n * factorial(n - 1)
+
+    private fun binom(n: Int, m: Int): Int = (factorial(n) / factorial(n-m) / factorial(m)).toInt()
+
 }
+
+
--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,21 @@
 package subtask2
 
-class SquareDecomposer {
 
-    // TODO: Complete the following function
+class SquareDecomposer {
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+
+        return null
     }
+
+    fun variants(a: Int): List<Pair<Int, Array<Int>>> = if (a == 1) listOf(1 to arrayOf(1)) else {
+        val first = variants(a - 1)
+        val second = a * a to arrayOf(a)
+        val last = first.map { it.first + second.first to it.second + second.second}
+        first + second + last
+    }
+}
+
+fun main() {
+//    SquareDecomposer().variants(9).forEach { println("""${it.first} - ${it.second.joinToString(" ")}""") }
+    SquareDecomposer().variants(8).last { it.first == 21 }.second.forEach { println(it) }
 }
